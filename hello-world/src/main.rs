c286591344149a9678bf0350fce42f4ff0efd9be
Jacob Howell <jhowell45@googlemail.com>
2023-03-05 14:10:33 +0000

    initial project.

---
+++ hello-world/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey There!")
+}
