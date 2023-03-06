a83c0c777109067d631a8b035cef771c0b21d3be
Jacob Howell <jhowell45@googlemail.com>
2023-03-06 23:46:54 +0000

    Finishing state.

--- shared-mutable-state/src/main.rs
+++ shared-mutable-state/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use std::sync::Mutex;
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // Mutex is necessary to mutate safetly across threads
+}
+
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {counter}") // <- response with counter
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // NOTE: web::Data created _outside_ of the HttpServer::new closure
+    // If created inside it risks being de-synced if modified, so must be created outside and cloned/moved in
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0)
+    });
+
+    HttpServer::new(|| {
+        // move counter into the closure
+        App::new()
+            .app_data(counter.clone()) // <- register created data
+            .route("/", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
