2717d662f7a3534d56ccd33751fccf43741bc993
Jacob Howell <jhowell45@googlemail.com>
2023-03-07 00:31:31 +0000

    Finished.

---
+++ configure/src/main.rs
@@ -0,0 +1,24 @@
+mod scoped;
+mod normal;
+
+use actix_web::{web, App, HttpResponse, HttpServer};
+use scoped::scoped_config;
+use normal::config;
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .configure(config)
+            .service(web::scope("/api").configure(scoped_config))
+            .route(
+                "/",
+                web::get().to(|| async { HttpResponse::Ok().body("/") }),
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
